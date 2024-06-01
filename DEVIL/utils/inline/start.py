58ff3fe41a0c9598320fbdbd7013b8063e8d0673
𝐃𝛆ᴠɪ֟፝‌꯭ʟ𝁂꯭꯭꯭֯ ᴋɪ꯭ɴʛ𝆺꯭𝅥༎ࠫ <136243912+itsdevxd@users.noreply.github.com>
2024-06-01 11:11:43 +0530

    Update start.py

--- DEVIL/utils/inline/start.py
+++ DEVIL/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/btw_devX")
+            InlineKeyboardButton(text=_["S_B_7"], url="https://telegra.ph/file/da362f9ced51c6b92c433.mp4")
         ],
     ]
     return buttons
